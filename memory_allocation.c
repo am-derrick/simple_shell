a26f08f0915c7ace2a2346ae52f0a8e914ffdc24
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-08-27 03:15:49 -0700

    Remove trailing whitespace

--- memory_allocation.c
+++ memory_allocation.c
@@ -1,11 +1,9 @@
-  
 #include "shell.h"
 
 /**
  * _realloc - reallocates a pointer to double the space
  * @ptr: pointer to the old array
  * @size: pointer to number of elements in the old array
- *
  * Return: pointer to the new array
  */
 char **_realloc(char **ptr, size_t *size)
